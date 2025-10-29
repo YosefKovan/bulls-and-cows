4875bd9bb27ba228d212e876e731c17f31e2c523
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2025-10-29 19:11:56 +0200

    adding the bull cow and score guess functions

--- game/logic.py
+++ game/logic.py
@@ -1,2 +1,23 @@
 
 #def init_state(secret: str, length: int, max_tries: int | None, unique_digits: bool, allow_leading_zero: bool):
+
+def bull(index, secret, guess):
+    """if the indexes equal it returns true"""
+    return secret[index] == guess[index]
+
+def cow(index, guess, secret):
+    """checks if the letter is in the secret"""
+    return guess[index] in secret
+
+def score_guess(secret: str, guess: str) -> tuple[int, int]:
+    """counts the bulls and counts and returns the results"""
+    bulls, cows = 0, 0
+
+    for index in range(0, len(guess)):
+
+        if bull(index, secret, guess):
+            bulls += 1
+        elif cow(index, secret, guess):
+            cows += 1
+
+    return bulls, cows
